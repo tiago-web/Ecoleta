2500afef9fc00b6e270cb8ac44437ffd9ffb6bf3
Tiago Soriano <tiago.ninjarj@gmail.com>
2020-06-06 17:45:55 -0400

    Finish image-upload feature between back and front end

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -30,6 +30,7 @@ const CreatePoint = () => {
 	const [items, setItems] = useState<Item[]>([]);
 	const [ufs, setUfs] = useState<string[]>([]);
 	const [cities, setCities] = useState<string[]>([]);
+	const [selectedFile, setSelectedFile] = useState<File>();
 
 	const [initialPosition, setInitialPosition] = useState<[number, number]>([
 		0,
@@ -135,16 +136,20 @@ const CreatePoint = () => {
 		const [latitude, longitude] = selectedPosition;
 		const items = selectedItems;
 
-		const data = {
-			name,
-			email,
-			whatsapp,
-			uf,
-			city,
-			latitude,
-			longitude,
-			items,
-		};
+		const data = new FormData();
+
+		data.append("name", name);
+		data.append("email", email);
+		data.append("whatsapp", whatsapp);
+		data.append("uf", uf);
+		data.append("city", city);
+		data.append("latitude", String(latitude));
+		data.append("longitude", String(longitude));
+		data.append("items", items.join(","));
+
+		if (selectedFile) {
+			data.append("image", selectedFile);
+		}
 
 		await api.post("points", data);
 
@@ -166,7 +171,7 @@ const CreatePoint = () => {
 					Cadastro do <br /> ponto de coleta
 				</h1>
 
-				<Dropzone />
+				<Dropzone onFileUploaded={setSelectedFile} />
 
 				<fieldset>
 					<legend>
